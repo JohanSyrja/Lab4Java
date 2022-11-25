eeb7ec35c9eb7271deff0f417e57e52610da270b
JohanSyrja <syrre00gmail.com>
2022-11-25 01:39:37 +0100

    uppgift 4 klar

--- src/Twoballs.java
+++ src/Twoballs.java
@@ -16,7 +16,6 @@ public class Twoballs {
     final static int UPDATE_FREQUENCY = 100;    // Global constant: fps, ie times per second to simulate
 
     public static void main(String[] args) {
-
         JFrame frame = new JFrame("No collisions!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Table table = new Table();
